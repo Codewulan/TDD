7ce0cbc9f6910a6c7735dcf274baabaf2efc92b1
qian.liu <liuqian@3ycloud.com>
2020-10-29 17:20:50 +0800

    单位换算：实现英尺、英寸之间的换算

---
+++ src/main/java/Length.java
@@ -0,0 +1,40 @@
+import static java.lang.String.format;
+
+/**
+ * @author paige
+ * @create 2020-10-29 17:04
+ */
+public class Length {
+    private int amountInInch;
+    private int amount;
+    private String unit;
+
+    public Length(int amount, String unit) {
+        this.amount = amount;
+        this.unit = unit;
+        if (unit.equals("Foot")) {
+            this.amountInInch = 12 * amount;
+        }
+        else {
+            this.amountInInch = amount;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return amountInInch == length.amountInInch;
+    }
+
+    @Override
+    public String toString() {
+        return format("%d (%s)", amount, unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ src/test/java/LengthTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author paige
+ * @create 2020-10-29 17:03
+ */
+public class LengthTest {
+    @Test
+    public void should_create_inch_with_given_amount() {
+        Length length = new Length(1, "Inch");
+    }
+
+    @Test
+    public void should_1_inch_equals_1_inch() {
+        assertThat(new Length(1, "Inch"), is(new Length(1, "Inch")));
+    }
+
+    @Test
+    public void should_display_internal_information_friendly() {
+        assertThat(new Length(1, "Inch").toString(), is("1 (Inch)"));
+        assertThat(new Length(1, "Foot").toString(), is("1 (Foot)"));
+    }
+
+    @Test
+    public void should_1_foot_equals_12_inches() {
+        assertThat(new Length(1, "Foot"), is(new Length(12, "Inch")));
+    }
+
+}
